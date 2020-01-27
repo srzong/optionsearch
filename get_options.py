0cce00456e4c14ef3d3cd2a8d6643308bf7e212d
rong <rzong@yahoo.com>
2020-01-27 09:00:51 -0800

    fix et calc

--- get_options.py
+++ get_options.py
@@ -3,6 +3,7 @@ import time
 from datetime import datetime
 import requests
 
+ARGS = {}
 
 CSS_RANGE = (0.165,0.4)
 CBS_RANGE = (0.05,0.16)
@@ -49,10 +50,10 @@ def printCandidates(candidates):
         tc_width_rank = candidate["Tc_width_rank"]
         et_width = candidate["Et_width"]
         et_width_rank = candidate["Et_width_rank"]
-        Et_ml = candidate["Et_ml"]
-        Et_ml_rank = candidate["Et_ml_rank"]
-        tc_ml = candidate["Tc_ml"]
-        tc_ml_rank = candidate["Tc_ml_rank"]
+        #Et_ml = candidate["Et_ml"]
+        #Et_ml_rank = candidate["Et_ml_rank"]
+        #tc_ml = candidate["Tc_ml"]
+        #tc_ml_rank = candidate["Tc_ml_rank"]
         Et_tc = candidate["Et_tc"]
         Et_tc_rank = candidate["Et_tc_rank"]
 
@@ -109,88 +110,70 @@ def get_chains(symbol):
     #print(rsp_json)
     return rsp_json
 
+def get_options(option_map, underlying):
+    results = []
+    expire_date = None
+    for expDate in option_map:
+        if expire_date:
+            if expDate != expire_date:
+                continue
+        else:
+            expire_date = expDate
+        bundle = option_map[expDate]
+        for strikePrice in bundle:
+            options = bundle[strikePrice]
+            for option in options:
+                description = option["description"]
+                print("description:", description)
+                delta = option["delta"]
+                strike = float(strikePrice)
+                if description.endswith("Put"):
+                    if underlying <= strike:
+                        print(f"skip put, underlying: {underlying} strike: {strike}")
+                        continue
+                elif description.endswith("Call"):
+                    if underlying >= strike:
+                        print(f"skip call, underlying: {underlying} strike: {strike}")
+                        continue
+                else:
+                    print(f"unexpected description: {description}")
+                    sys.exit(1)
+                price = (option["bid"] + option["ask"])/2.0
+                item = {"description": description, "delta": delta, "strike": strike, "price": price}
+                """
+                if  delta >= PSS_RANGE[0] and delta <= PSS_RANGE[1]:
+                    pss_options.append(item)
+                elif delta >= PBS_RANGE[0] and delta <= PBS_RANGE[1]:
+                    pbs_options.append(item)
+                else:
+                    print(f"skip put option: {description} delta: {delta}")
+                """
+                results.append(item)
+    return results
+    
+
 def get_contracts(chains):
+    underlying = chains["underlyingPrice"]
     putMap = chains["putExpDateMap"]
     callMap = chains["callExpDateMap"]
     
-    css_options = []
-    cbs_options = []
-    pss_options = []
-    pbs_options = []
-    num_calls = 0
-    num_puts = 0
-    
-    expire_date = None
-    for expDate in putMap:
-        if expire_date:
-            if expDate != expire_date:
-                continue
-        else:
-            expire_date = expDate
-        bundle = putMap[expDate]
-        for strikePrice in bundle:
-            options = bundle[strikePrice]
-            for option in options:
-                num_puts += 1
-                description = option["description"]
-                delta = option["delta"]
-                strike = float(strikePrice)
-                price = (option["bid"] + option["ask"])/2.0
-                if  delta >= PSS_RANGE[0] and delta <= PSS_RANGE[1]:
-                    pss_options.append({"description": description, "delta": delta, "strike": strike, "price": price})
-                elif delta >= PBS_RANGE[0] and delta <= PBS_RANGE[1]:
-                    pbs_options.append({"description": description, "delta": delta, "strike": strike, "price": price})
-                else:
-                    print(f"skip put option: {description} delta: {delta}")
-    
-    for expDate in callMap:
-        if expire_date:
-            if expDate != expire_date:
-                continue
-        else:
-            expire_date = expDate
-        bundle = callMap[expDate]
-        for strikePrice in bundle:
-            options = bundle[strikePrice]
-            for option in options:
-                num_calls += 1
-                description = option["description"]
-                delta = option["delta"]
-                strike = float(strikePrice)
-                price = (option["bid"] + option["ask"])/2.0
-                if delta >= CSS_RANGE[0] and delta <= CSS_RANGE[1]:
-                    css_options.append({"description": description, "delta": delta, "strike": strike, "price": price})
-                elif delta >= CBS_RANGE[0] and delta <= CBS_RANGE[1]:
-                    cbs_options.append({"description": description, "delta": delta, "strike": strike, "price": price})
-                else:
-                    print(f"skip call option: {description} delta: {delta}")
-    
-    print("num_puts:", num_puts)
-    print("num_calls:", num_calls)
-    total = len(css_options) + len(cbs_options) + len(pss_options) + len(pbs_options)
-    print("total accepted:", total)
-    print("contracts...")
-    for contract_list in (css_options, cbs_options, pss_options, pbs_options):
-        for contract in contract_list:
-            description = contract["description"]
-            strike = contract["strike"]
-            delta = contract["delta"]
-            price = contract["price"]
-            print(f"{description} -- price: {price:.2f} strike: {strike:.2f} delta: {delta:.2f}")
-        print("------")
-    return {"css": css_options, "cbs": cbs_options, "pss": pss_options, "pbs": pbs_options}   
+    put_options = get_options(putMap, underlying)
+    print(f"got {len(put_options)} put options")
+    call_options = get_options(callMap, underlying)
+    print(f"got {len(call_options)} call options")
+
+    return {"underlying": underlying, "call": call_options, "put": put_options}   
 
 
 def get_candidates(contracts):
     candidates = []
-    cs_list = contracts["css"]
-    cb_list = contracts["cbs"]
-    pb_list = contracts["pbs"]
-    ps_list = contracts["pss"]
-    for cs in cs_list:
-        for cb in cb_list:
-            for pb in pb_list:
-                for ps in ps_list:
+    call_list = contracts["call"]
+    put_list = contracts["put"]
+    
+    for cs in call_list:
+        for cb in call_list:
+            for pb in put_list:
+                for ps in put_list:
                     candidate = {"cs": cs, "cb": cb, "pb": pb, "ps": ps}
                     css = cs["strike"]
                     cbs = cb["strike"]
@@ -204,9 +187,25 @@ def get_candidates(contracts):
                     cbd = cb["delta"]
                     pbd = pb["delta"]
                     psd = ps["delta"]
-                    if pss == pbs or cbs == css or pss == css:
-                        print("strikes overlap, skipping")
+                    if pbs >= pss:
+                        #print("pbs >= pss, skipping")
                         continue
+                    if pss >= css:
+                        #print("pss >= css, skipping")
+                        continue
+                    if css >= cbs:
+                        #print("css >= cbs, skipping")
+                        continue
+                    if pbp >= psp:
+                        #print("pbp >= psp, skipping")
+                        continue
+                    if cbp >= csp:
+                        #print("cbp >= csp, skipping")
+                        continue
+                    print(f"cs: {cs['description']}")
+                    print(f"cb: {cb['description']}")
+                    print(f"ps: {ps['description']}")
+                    print(f"pb: {pb['description']}")
                     Tc = csp - cbp + psp - pbp
                     width = ((cbs - css) + (pss - pbs)) * 0.5
                     Tc_width = Tc / width
@@ -246,11 +245,10 @@ def get_candidates(contracts):
                     candidate["Tc_ml"] = Tc_ml
                     candidate["Et_tc"] = Et_tc
 
-                    """
+                
                     print(f"==================")
                     print(f"{css:.3f}/{cbs:.3f}/{pss:.3f}/{pbs:.3f}")
-                    """
-                    """
+                    
                     print(f"csp: {csp:.3f}")
                     print(f"psp: {psp:.3f}")
                     print(f"cbp: {cbp:.3f}")
@@ -283,44 +281,61 @@ def get_candidates(contracts):
                     print(f"Et/ml: {Et_ml:.3f}")
                     print(f"Et/width: {Et_width:.3f}")
                     print(f"Et/Tc: {Et/Tc:.3f}")
-                    """
-
+                    
                     #if True or Et > 0.1 and Tc > width * 0.3334:                   
-                    #if Et_width > 0.1 and Tc_width > 0.3334:
-                    if Et > 3.5:
+                    if Et_width > 0.01 and Tc_width > 0.3334:
+                        #if True or Et > 3.5:
                         #print("adding candidate")
                         candidates.append(candidate)
     return candidates
         
 
+def print_usage():
+    print("usage: python get_options.py [--skip-delta] SYM")
 
 
 #
 # Main
 #
 if len(sys.argv) < 2 or sys.argv[1] in ("-h", "--help"):
-    print("usage: python get_options.py SYM")
+    print_usage()
     sys.exit(1)
-symbol = sys.argv[1]
+
+symbols = []
+ARGS["skip_delta"] = False
+for argn in range(1, len(sys.argv)):
+    print("got arg:", sys.argv[argn])
+    argval = sys.argv[argn]
+    if argval.startswith('-'):
+        if argval == "--skip-delta":
+            ARGS["skip_delta"] = True
+        else:
+            print_usage()
+            sys.exit(1)
+    else:
+        symbols.append(argval)
+if not symbols:
+    print_usage()
+    sys.exit(1)
+symbol = symbols[0]
 print("getting symbol:", symbol)
 chains = get_chains(symbol)
 if not chains:
     sys.exit(1)
 contracts = get_contracts(chains)
 
-for k in contracts:
+for k in ("call", "put"):
     options = contracts[k]
     print(k, len(options))
     for option in options:
         print(option["description"], option["delta"])
 
-for k in contracts:
+for k in ("call", "put"):
     options = contracts[k]
     if len(options) == 0:
         print("no candidates!")
         sys.exit(0)
 
-print(f"got {len(contracts)} contracts")
 candidates = get_candidates(contracts)
 print("got", len(candidates), "candidates")
 print("======================")
