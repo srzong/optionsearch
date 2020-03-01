f08fd7fd8f6f2d4682ac545f5bed4ef65df92c53
rong <rzong@yahoo.com>
2020-03-01 12:34:38 -0800

    add data only update

--- get_all.py
+++ get_all.py
@@ -2,19 +2,17 @@ import subprocess
 import sys
 import os
 
-def get_options(symbol, option=None, outdir=None, reload=False):
+def get_options(symbol, option=None, outdir=None, refresh_option=None):
     args = ["python", "get_options.py"] 
 
-    if reload:
-        args.append("--reload") 
+    if refresh_option:
+        args.append(refresh_option) 
 
     if option:
         args.append(option)
     args.append(symbol)
 
-    if not outdir:
-        outdir = "out"
-    if not os.path.isdir(outdir):
+    if outdir and not os.path.isdir(outdir):
         os.mkdir(outdir)
     
     fout = open("tmp.txt", "w")
@@ -30,13 +28,13 @@ def get_options(symbol, option=None, outdir=None, reload=False):
 # main
 #
 if len(sys.argv) <= 2 or sys.argv[1] in ('-h', '--help'):
-    print("usage: python get_all.py [--reload] [stocklist_file] [out_dir]")
+    print("usage: python get_all.py [--reload|--useold|--dataonly] [stocklist_file] [out_dir]")
     sys.exit(0)
 
 argnum = 1
-reload = False
-if sys.argv[argnum] == "--reload":
-    reload = True
+refresh_option = None   # or "--reload" or "--useold" or "--dataonly"
+if sys.argv[argnum] in ("--reload", "--useold", "--dataonly"):
+    reresh_otion = sys.argv[argnum]
     argnum += 1
 stocklist_file = sys.argv[argnum]
 argnum += 1
@@ -44,10 +42,13 @@ if not os.path.isfile(stocklist_file):
     print(f"{stocklist_file} not found")
     sys.exit(1)
 
-folder = sys.argv[argnum]
+folder = None
+if len(sys.argv) > argnum:
+    folder = sys.argv[argnum]
+    print(f"argnum: {argnum} folder: {folder}")
 
-if not os.path.isdir(folder):
-    os.mkdir(folder)
+    if not os.path.isdir(folder):
+        os.mkdir(folder)
 
 symbols = []
 with open(stocklist_file, "r") as f:
@@ -63,21 +64,26 @@ with open(stocklist_file, "r") as f:
             line = f.readline()
             continue
         print(symbol)
-        """
-        # do ic
-        outdir = folder + "/ic"
-        rc = get_options(symbol, outdir=outdir, reload=reload)
+        
+        # do ic (or just update data files in case of reload=--dataonly)
+        outdir = None
+        if folder:
+            outdir = folder + "/ic"
+        rc = get_options(symbol, outdir=outdir, refresh_option=refresh_option)
         if rc == 0:
             symbols.append(symbol)    
-        """
-        # do puts
-        outdir = folder + "/puts"
-        rc = get_options(symbol, option="--puts", outdir=outdir, reload=reload) 
-        #print(f"{cnt}: {len(fields)}")
         
-        # do calls
-        outdir = folder + "/calls"
-        rc = get_options(symbol, option="--calls", outdir=outdir, reload=reload)
+        if refresh_option != "--dataonly":
+            # do puts
+            if folder:
+                outdir = folder + "/puts"
+            rc = get_options(symbol, option="--puts", outdir=outdir, refresh_option=refresh_option) 
+            #print(f"{cnt}: {len(fields)}")
+        
+            # do calls
+            if folder:
+                outdir = folder + "/calls"
+            rc = get_options(symbol, option="--calls", outdir=outdir, refresh_option=refresh_option)
         
         line = f.readline()
         
---
+++ updatedata.sh
@@ -0,0 +1,2 @@
+python get_all.py --dataonly xstocks.csv
+python get_all.py --dataonly xetfs.csv 
